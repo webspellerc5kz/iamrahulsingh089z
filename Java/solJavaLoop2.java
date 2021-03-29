f8c89e2466b0cda899f5f3d8149621ec124b2700
webspellerc5kz <congjiuhuan77382@163.com>
2021-03-29 17:28:36 +0800

    Create solJavaLoop2.java

---
+++ Java/solJavaLoop2.java
@@ -0,0 +1,57 @@
+/* We use the integers a, b, and n to create the following series:
+
+You are given q queries in the form of a, b, and n. For each query, print the series corresponding to the given a, b, and n values as a single line of n space-separated integers.
+
+Input Format
+
+The first line contains an integer,q, denoting the number of queries.
+Each line i of the q subsequent lines contains three space-separated integers describing the respective ai, bi, and ni values for that query.
+
+Constraints
+
+Output Format
+
+For each query, print the corresponding series on a new line. Each series must be printed in order as a single line of  space-separated integers.
+
+Sample Input
+
+2
+0 2 10
+5 3 5
+
+Sample Output
+
+2 6 14 30 62 126 254 510 1022 2046
+8 14 26 50 98
+
+*/
+
+/* =================================================================================================================
+                                                  SOLUTION
+   ================================================================================================================= */
+
+import java.util.Scanner;
+import java.lang.Math;
+
+class Solution{
+    
+    public static void main(String []arg){
+        
+        Scanner in = new Scanner(System.in);
+        
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            int result = a;
+            
+            for(int j=0; j<n; j++){
+                result+=(Math.pow(2,j)*b);
+                System.out.print(result+" ");
+            }
+            System.out.println();
+        }
+        in.close();
+    }
+}
